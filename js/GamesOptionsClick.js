b5feafe7b7a3ddea1b8c6247450e699b9feceda5
MintyMonster <78669960+MintyMonster@users.noreply.github.com>
2024-03-20 00:35:31 +0000

    Project page responsiveness

--- js/GamesOptionsClick.js
+++ js/GamesOptionsClick.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 
-    var options = [$(".slide-options-games-one"), $(".slide-options-games-two"), $(".slide-options-games-three"), $(".slide-options-games-four")];
-    var containers = [$(".slide-text-games-container-one"), $(".slide-text-games-container-two"), $(".slide-text-games-container-three"), $(".slide-text-games-container-four")];
+    var options = [$(".slide-options-games-one"), $(".slide-options-games-two"), $(".slide-options-games-three"), $(".slide-options-games-four"), $(".slide-options-games-five"), $(".slide-options-games-six")];
+    var containers = [$(".slide-text-games-container-one"), $(".slide-text-games-container-two"), $(".slide-text-games-container-three"), $(".slide-text-games-container-four"), $(".slide-text-games-container-five"), $(".slide-text-games-container-six")];
     var hide = "hide-text";
     var show = "show-text";
 
--- js/SoftwareOptionsClick.js
+++ js/SoftwareOptionsClick.js
@@ -2,13 +2,9 @@ $(document).ready(function() {
 
     var optionOne = $(".slide-options-software-one");
     var optionTwo = $(".slide-options-software-two");
-    var optionThree = $(".slide-options-software-three");
-    var optionFour = $(".slide-options-software-four");
 
     var containerOne = $(".slide-text-software-container-one");
     var containerTwo = $(".slide-text-software-container-two");
-    var containerThree = $(".slide-text-software-container-three");
-    var containerFour = $(".slide-text-software-container-four");
 
     var hide = "hide-text";
     var show = "show-text";
@@ -18,38 +14,13 @@ $(document).ready(function() {
             $(containerOne).removeClass(hide).addClass(show);
             
             $(containerTwo).removeClass(show).addClass(hide);
-            $(containerThree).removeClass(show).addClass(hide);
-            $(containerFour).removeClass(show).addClass(hide);
         }
     });
 
     $(optionTwo).click(function() {
         if($(containerTwo).hasClass(hide)){
             $(containerTwo).removeClass(hide).addClass(show);
-
-            $(containerThree).removeClass(show).addClass(hide);
-            $(containerFour).removeClass(show).addClass(hide);
             $(containerOne).removeClass(show).addClass(hide);
-        }
-    });
-
-    $(optionThree).click(function() {
-        if($(containerThree).hasClass(hide)){
-            $(containerThree).removeClass(hide).addClass(show);
-
-            $(containerTwo).removeClass(show).addClass(hide);
-            $(containerFour).removeClass(show).addClass(hide);
-            $(containerOne).removeClass(show).addClass(hide);
-        }
-    });
-
-    $(optionFour).click(function() {
-        if($(containerFour).hasClass(hide)){
-            $(containerFour).removeClass(hide).addClass(show);
-
-            $(containerOne).removeClass(show).addClass(hide);
-            $(containerTwo).removeClass(show).addClass(hide);
-            $(containerThree).removeClass(show).addClass(hide);
         }
     });
 
